27e343321f38d2957185004435f5be602c4a0c1c
CatKhakis <violet@funkycat.media>
2026-09-19 20:12:38 -0500

    initialize

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <input
+    type="url"
+    name="url"
+    id="url"
+    placeholder="http://localhost:3000"
+    pattern="http://.*"
+    size="30"
+    required />
+  <input
+    type="button"
+    value="connect"
+  />
+</template>
+
+<style scoped></style>
